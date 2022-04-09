87cd653cea3cb93f748163a5a0f3b833ab8501a2
rizadogukankosar <rizadogukankosar@gmail.com>
2022-04-09 19:19:02 +0300

    Toplama özelliği eklendi

---
+++ calculator.py
@@ -0,0 +1,10 @@
+
+#Rıza Doğukan Koşar 19253509
+
+def add(a, b):
+    return a + b
+
+print("Lütfen toplamak istediğiniz sayıları girin")
+number1 = int(input("İlk sayınızı girin: "))
+number2 = int(input("ikinci sayınızı girin: "))
+print(number1, " + ", number2, " = ", add(number1,number2))
